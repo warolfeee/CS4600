9be0a1517a02b3be8db1c13e1dd1200e79f8ce5c
Shayla Basile <shaylawilkinson24@gmail.com>
2020-04-05 14:05:47 -0600

    Working model.

--- tensor.py
+++ tensor.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
 import pickle
+import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 
 X = pickle.load(open('X.pickle', 'rb'))
 Y = pickle.load(open('Y.pickle', 'rb'))
 
-X = X/255.0
+X = np.array(X/255.0)
+Y = np.array(Y)
 
 model = Sequential()
 model.add(Conv2D(64, (3, 3), input_shape = X.shape[1:]))
