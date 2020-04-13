dfef0be11c2623b1cd57bfbd054a9f3d896da71b
Shayla Basile <shaylawilkinson24@gmail.com>
2020-04-13 17:12:19 -0600

    Adding parser.

---
+++ parseBirdId.py
@@ -0,0 +1,16 @@
+def ParseBirdId():
+    filename = input("")
+    fin = open(filename, "r")
+    for line in fin:
+        for i in range(len(line)-5):
+            testLine = line[i] + line[i+1] + line[i+2] + line[i+3] + line[i+4] 
+            if testLine == "\"id\":":
+                birdId=""
+                k = i+7
+                while line[k] != "\"":
+                    birdId += line[k]
+                    k+=1
+                print(birdId)
+    fin.close()
+
+ParseBirdId()             
